e3f12108f0a7be3e251d002c810763cc99993efe
Anton Katsarov <anton@katsarov.org>
2014-01-09 12:17:31 +0200

    * Disabeling GoogleMail

--- scripts/modify_features.pl
+++ scripts/modify_features.pl
@@ -16,6 +16,7 @@ my @feature_lists = Cpanel::Features::get_feature_lists();
 my @addonfeatures = Cpanel::Features::load_addon_feature_names();
 @stop_features = grep {/^(cgpro|itoolabs)\_/} @addonfeatures;
 my @cmail_stop_features = grep {/^noncgpro\_/} @addonfeatures;
+push @cmail_stop_features, "googleappswizard";
 my @cmail_start_features = ("cgpro_backup", "cgpro_dnssrv", "cgpro_helpers", "cgpro_autoresponders", "cgpro_pronto", "cgpro_autoloaders", "cgpro_groupware", "cgpro_forwardersemail", "cgpro_spamassassin", "cgpro_maillist", "cgpro_groupmail", "cgpro_filters", "cgpro_defaultaddress", "cgpro_emailarchive", "cgpro_pronto_html5", "cgpro_emailverify", "cgpro_account_defaults", "cgpro_xmpp_clients", "cgpro_popaccts", "cgpro_contacts", "cgpro_xmpp_history", "cgpro_xmpp_roster", "cgpro_account_types", "cgpro_rpop", "cgpro_airsync", "cgpro_prontodrive", "cgpro_prontodrive_settings", "cgpro_legacy_webmail", "cgpro_emailtrace", "cgpro_changemx", "cgpro_emailauth", "cgpro_csvimport", "cgpro_migration_wizard", "cgpro_energy_webmail");
 foreach my $feature_list_name (@feature_lists) {
     if ($feature_list_name ne 'disabled') {
@@ -36,7 +37,7 @@ foreach my $feature_list_name (@feature_lists) {
 		    'value' => '1',
 		    'list' => $feature_list_name . "-cMailPro"
 						 ));
-		print "Stopping feature \"" . $feature . "\" in \"".$feature_list_name."-cMailPro\" by default\n";
+		print "Starting feature \"" . $feature . "\" in \"".$feature_list_name."-cMailPro\" by default\n";
 	    }
 	}
 	foreach my $feature (@stop_features) {
